b7eafca405c6342f9e6b4e50d3ccb198aa9efffb
Edivan Vicente <edivan.vicenteads@gmail.com>
2026-09-23 09:41:59 -0300

    imp-logica-excluir

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 function addTask() {
 
     let taskInput = document.getElementById("taskInput");
-    let taskText  = taskInput.value;
+    let taskText  = taskInput.value.trim();
     let taskList  = document.getElementById("taskList");
-   
+    let mensagem = document.getElementById("emptyMessage");
+
+    if (taskText === "") {
+        alert("Digite uma tarefa!");
+        return;
+    }
+   function checkEmptyList(){
+    taskList.children.length;
+    
+    
+    if(checkEmptyList === 0){
+        mensagem.style.display ="none"
+    }else {
+        mensagem.style.display = "block"
+    }
+   }
     // cria o li
     let newTask = document.createElement("li");
 
@@ -18,15 +33,20 @@ function addTask() {
     // monta o li por dentro (ordem = ordem na tela)
     newTask.appendChild(taskCheckbox);
     newTask.appendChild(taskSpan);
-    // monta a lixeira 
-   // monta a lixeira
-2
+    taskCheckbox.addEventListener("change", function(){
+        taskSpan.classList.toggle("completed")
+    });
+
 let taskButton = document.createElement("button");
-3
+
 taskButton.textContent = "🗑"; // o que aparece no botão
-4
+
 taskButton.className = "thrash-icon"; // o nome usado pelo CSS
-5
+
+taskButton.addEventListener("click", function() {
+    taskList.removeChild(newTask);
+});
+
 newTask.appendChild(taskButton);
     // entrega na tela
     taskList.appendChild(newTask);
